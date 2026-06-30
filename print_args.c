a8bc3e5cea486267e3d693cbf7775ba4a7a34de1
IanBubier <ianbubier@gmail.com>
2026-06-30 09:47:47 -0700

    Removed string.h use

--- print_args.c
+++ print_args.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char *argv[]){
-    for (int n = 0; n < argc; n++)
-        if (strncmp(argv[n], "-", 1) == 0) {
+int main(int argc, char *argv[]) {
+    for (int n = 0; n < argc; n++) {
+        if (argv[n][0] == '-') {
             printf("%s\n", argv[n]);
+        }
     }
+
     return 0;
 }
